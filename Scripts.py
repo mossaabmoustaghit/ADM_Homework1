0b6f060ab4a648172f377c327e4cb426c9200d9d
mossaabmoustaghit <80361333+mossaabmoustaghit@users.noreply.github.com>
2021-10-06 19:24:35 +0200

    Update Scripts.py

--- Scripts.py
+++ Scripts.py
@@ -1175,6 +1175,57 @@ if __name__ == '__main__':
 
 # Number Line Jumps (kangaroo)
 
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+def kangaroo(x1, v1, x2, v2):
+    # Write your code here
+    xdiff=x1-x2
+    vdiff=v2-v1
+    if (x1==x2) and (v1==v2):
+        return ('YES')
+    elif x1!=x2 and (v1==v2):
+        return ('NO')
+    elif (xdiff+vdiff)%vdiff==0 and (xdiff+vdiff)/vdiff>0:
+        return ('YES')
+    else:
+        return ('NO')
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+    
+    
+    
 
 
 
